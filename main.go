c85e104bb2d6b98165813aad1ea009e829658fd1
Haroon Albar <haroon.albar@functionary.co>
2024-04-24 14:00:06 +0530

    interface in go

--- main.go
+++ main.go
@@ -5,8 +5,25 @@ import (
 	"math"
 )
 
+type Abster interface{
+  abs() float64
+}
+// interface can be used to handle dynamic functions
+// here there are 2 types of abs() one that takes MyFloat and other that takes *Vertex
+// and Abster interface handles both abs methods.
+
 type Vertex struct {
 	X, Y float64
+}
+
+type MyFloat float64
+
+func (f MyFloat) abs() float64 {
+  if(f < 0){
+    return -float64(f)
+  }else{
+    return float64(f)
+  }
 }
 
 func (ver *Vertex) abs() float64 {
@@ -20,15 +37,21 @@ func (v *Vertex) change() {
 }
 
 func main() {
-	vertex := Vertex{3, 4}
+  var a Abster
+  f := MyFloat(-0.111)
+  a = f
+	fmt.Println(a.abs())
 
-	fmt.Println(vertex.abs())
+	vertex := Vertex{3, 4}
+  a = &vertex
+
+	fmt.Println(a.abs())
   // outupt will be 5
 
   vertex.change()
   // here change is is a pointer reciever so it can change the value of the reciever itself
 
-	fmt.Println(vertex.abs())
+	fmt.Println(a.abs())
   // outupt changes to 19.something snice the reciever value changed
 
 }
